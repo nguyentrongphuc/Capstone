48ab418f405b4ceb68635941f501b1597cb22e15
Phuc Nguyen <mr.nguyentrongphuc@gmail.com>
2023-07-20 00:49:22 -0700

    fix error

--- src/backend/database/models.py
+++ src/backend/database/models.py
@@ -9,14 +9,14 @@ def setup_db(app):
         if not database_exists(app.config['DB_PATH']):
             create_database(app.config["DB_PATH"])
 
-    print(f'DB_PATH {app.config["DB_PATH"]}')
+        print(f'DB_PATH {app.config["DB_PATH"]}')
 
-    print(f'DB_HOST {app.config["DB_HOST"]}')
+        print(f'DB_HOST {app.config["DB_HOST"]}')
 
-    print(f'DB_USER {app.config["DB_USER"]}')
-    print(f'DB_PASSWORD {app.config["DB_PASSWORD"]}')
+        print(f'DB_USER {app.config["DB_USER"]}')
+        print(f'DB_PASSWORD {app.config["DB_PASSWORD"]}')
 
-    print(f'DB_NAME {app.config["DB_NAME"]}')
+        print(f'DB_NAME {app.config["DB_NAME"]}')
 
     db.app = app
     db.init_app(app)
