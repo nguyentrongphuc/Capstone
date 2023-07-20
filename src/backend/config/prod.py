8d0e8736a93b73145c3d14fd0e150e1261ea208d
Phuc Nguyen <mr.nguyentrongphuc@gmail.com>
2023-07-20 01:44:40 -0700

    fix error

--- src/backend/config/prod.py
+++ src/backend/config/prod.py
@@ -3,15 +3,15 @@ import os
 TESTING = False
 
 # Connect to the database 
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
-DB_USER = os.getenv('DB_USER', 'postgres')  
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'P%40ssw0rd')  
+DB_HOST = os.environ('DB_HOST', '127.0.0.1:5432')  
+DB_USER = os.environ('DB_USER', 'postgres')  
+DB_PASSWORD = os.environ('DB_PASSWORD', 'P%40ssw0rd')  
 
 
 # DB_HOST = os.getenv('DB_HOST', 'vehicleinfo.cpozezvwnhgr.us-east-2.rds.amazonaws.com:5432')  
 # DB_USER = os.getenv('DB_USER', 'postgres')  
 # DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')  
-DB_NAME = os.getenv('DB_NAME', 'vehicleinfo')  
+DB_NAME = os.environ('DB_NAME', 'vehicleinfo')  
 DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 
 # TODO IMPLEMENT DATABASE URL
