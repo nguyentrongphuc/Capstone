ec8b8fbbe610853cf4eb34a95d9b267a3e846dcc
Phuc Nguyen <mr.nguyentrongphuc@gmail.com>
2023-07-21 17:13:14 -0700

    updae token

--- src/backend/main_test.py
+++ src/backend/main_test.py
@@ -4,8 +4,6 @@ import json
 import main
 
 from database.models import db_drop_and_create_all, setup_db, VehicleMake, VehicleModel
-
-
 class CapstoneTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
